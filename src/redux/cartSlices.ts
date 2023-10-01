119af3c407903046159709d5c29638dd71b7b295
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-10-01 19:45:28 +0700

    feat: create slices and store for redux

---
+++ src/redux/cartSlices.ts
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface CartItem {
+  id: number;
+  title: string;
+  images: string;
+  price: number;
+  quantity: number;
+}
+
+interface CartState {
+  cart: CartItem[];
+}
+
+const initialState: CartState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const itemInCart = state.cart.find((item) => item.id === action.payload.id);
+      if (itemInCart) {
+        itemInCart.quantity++;
+      } else {
+        state.cart.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    incrementQuantity: (state, action) => {
+      const item = state.cart.find((item) => item.id === action.payload);
+      if (item) {
+        item.quantity++;
+      }
+    },
+    decrementQuantity: (state, action) => {
+      const item = state.cart.find((item) => item.id === action.payload);
+      if (item && item.quantity > 1) {
+        item.quantity--;
+      }
+    },
+    removeItem: (state, action) => {
+      const removeItem = state.cart.filter((item) => item.id !== action.payload);
+      state.cart = removeItem;
+    },
+  },
+});
+
+export default cartSlice.reducer;
+export const { addToCart, incrementQuantity, decrementQuantity, removeItem } = cartSlice.actions;
